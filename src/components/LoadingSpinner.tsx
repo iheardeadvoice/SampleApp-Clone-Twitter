487e408b8fd692e832b70472dd2d7e42f9874749
iheardeadvoice <22ip235@stud.scc>
2026-03-30 12:09:15 +0300

    React Sprint 1: создание проекта, сервис UsersService, компонент Home с таблицей Material UI

---
+++ src/components/LoadingSpinner.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Box, CircularProgress, Typography } from '@mui/material';
+
+export const LoadingSpinner = () => (
+  <Box display="flex" flexDirection="column" alignItems="center" justifyContent="center" minHeight="200px" gap={2}>
+    <CircularProgress />
+    <Typography color="text.secondary">Загрузка...</Typography>
+  </Box>
+);
